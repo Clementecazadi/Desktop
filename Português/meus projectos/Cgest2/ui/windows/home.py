cdedd3afcc9be2a1e6dcd337e01d665e8f98be3c
Clemente Alberto Cazadi <clementecazadi@gmail.com>
2022-11-25 21:06:01 +0100

    Acabei o bloco estoque em baixa.

--- Português/meus projectos/Cgest2/ui/windows/home.py
+++ Português/meus projectos/Cgest2/ui/windows/home.py
@@ -69,6 +69,7 @@ class Block_dish_in_high(QFrame):
         self.block_layout.addWidget(self.dish_name)
         self.block_layout.addLayout(self.elements)
 
+        
 class Block_dish_manager(QWidget):
     def __init__(self):
         super(Block_dish_manager, self).__init__()
@@ -133,6 +134,8 @@ class Block_dish_manager(QWidget):
             self.ui_layers.setItemWidget(item, widget)
             item.setSizeHint(widget.sizeHint())
 
+
+# Criando o bloco pedidos activos        
 class Block_order_active(QFrame):
     def __init__(self, dish_name:str = 'Desconhecido', table_num:int = 0, enter_order:str = '23'):
         super(Block_order_active, self).__init__()
@@ -237,6 +240,140 @@ class Block_order_manager(QWidget):
             self.ui_layers.setItemWidget(item, widget)
             item.setSizeHint(widget.sizeHint())
 
+# Criando o bloco stock em baixo.
+class Block_low_stock(QFrame):
+    def __init__(self, product_name:str = 'Desconhecido', stock:float = 0, weight_total:float = 0):
+        super(Block_low_stock, self).__init__()
+        self.setStyleSheet('background-color: qlineargradient'
+        '(spread:pad, x1:0.519, y1:0, x2:0.5, y2:0.835, stop:0 rgba(128, 71, 213, 255), stop:1 '
+        'rgba(43, 19, 77, 255));')
+        # Criando elementos do bloco...
+        self.product_name = QLabel(product_name)
+        self.product_name.setAlignment(Qt.AlignHCenter)
+        self.product_name.setStyleSheet('background-color: #00000000; color: #ffffff')
+        font = get_font('berkshire.ttf', font_size=24)
+        font.setItalic(True)
+        self.product_name.setFont(font)
+        self.product_name.setWordWrap(True)
+        self.stock = AdvencedLabel(f'Peso total: {stock} de {weight_total}Kg', font_size=14)
+        self.stock.setStyleSheet('background-color: #00000000; color: #ffffff')
+        percent =int((stock * 100) / weight_total)
+        self.percent_label_1 = AdvencedLabel('Percentagem:', font_size=14)
+        self.percent_label_1.setStyleSheet('background-color: #00000000; color: #ffffff')
+        self.percent_progress = QProgressBar()
+        self.percent_progress.setMaximum(100)
+        self.percent_progress.setMinimum(0)
+        self.percent_progress.setValue(percent)
+        self.percent_progress.setTextVisible(False)
+        self.percent_progress.setMaximumHeight(5)
+        self.percent_progress.setStyleSheet('''
+                QProgressBar::chunk {
+                    background-color: #fedb04;
+	                border-radius: 2px
+                };
+                border-radius: 2px;
+        ''')
+        self.percent_label_2 = AdvencedLabel(f'{percent}%', font_size= 14)
+        self.percent_label_2.setStyleSheet('background-color: #00000000; color: #ffffff')
+
+        # Criando layout para os elementos de percentagem
+        self.percent_layout = QHBoxLayout()
+        self.percent_layout.setContentsMargins(0, 0, 0, 0)
+        self.percent_layout.setSpacing(10)
+        # Adicionando elementos TAX
+        self.percent_layout.addWidget(self.percent_label_1)
+        self.percent_layout.addWidget(self.percent_progress)
+        self.percent_layout.addWidget(self.percent_label_2)
+        # Criando layout para todos elementos que não sejam o titulo do produto
+        self.elements = QVBoxLayout()
+        self.elements.setContentsMargins(0, 0, 0, 0)
+        self.elements.setSpacing(5)
+        # Adicionando elementos no layout.
+        self.elements.addWidget(self.stock)
+        self.elements.addLayout(self.percent_layout)
+        # Criando layout para o bloco inteiro
+        self.block_layout = QVBoxLayout(self)
+        self.block_layout.setSpacing(10)
+        self.block_layout.setContentsMargins(10, 10, 10, 10)
+        # Trecho de codigo em suspenção
+        """"
+        if len(product_name) > 10:
+            self.block_layout.setSpacing(10)
+            self.block_layout.setContentsMargins(10, 10, 10, 10)
+        else:
+            self.block_layout.setSpacing(10)
+            self.block_layout.setContentsMargins(10, 20, 10, 10)
+        """
+        # Adicionando elementos ao layout
+        self.block_layout.addWidget(self.product_name)
+        self.block_layout.addLayout(self.elements)
+
+
+class Block_stock_manager(QWidget):
+    def __init__(self):
+        super(Block_stock_manager, self).__init__()
+        self.ui_layers = QListWidget()
+        self.ui_layers.setViewMode(QListWidget.ListMode)
+        self.ui_layers.setSpacing(5)
+        self.ui_layers.setMovement(QListWidget.Static)
+        self.ui_layers.setWordWrap(True)
+        self.ui_layers.setVerticalScrollMode(QListWidget.ScrollPerPixel)
+        self.ui_layers.setStyleSheet(""
+            " QScrollBar:vertical {\n"
+            "	border: none;\n"
+            "    background: rgb(52, 59, 72);\n"
+            "    width: 14px;\n"
+            "    margin: 21px 0 21px 0;\n"
+            "	border-radius: 0px;\n"
+            " }\n"
+            " QScrollBar::handle:vertical {	\n"
+            "	background: rgb(85, 170, 255);\n"
+            "    min-height: 25px;\n"
+            "	border-radius: 7px\n"
+            " }\n"
+            " QScrollBar::add-line:vertical {\n"
+            "     border: none;\n"
+            "    background: rgb(55, 63, 77);\n"
+            "     height: 20px;\n"
+            "	border-bottom-left-radius: 7px;\n"
+            "    border-bottom-right-radius: 7px;\n"
+            "     subcontrol-position: bottom;\n"
+            "     subcontrol-origin: margin;\n"
+            " }\n"
+            " QScrollBar::sub-line:vertical {\n"
+            "	border: none;\n"
+            "    background: rgb(55, 63"
+                                    ", 77);\n"
+            "     height: 20px;\n"
+            "	border-top-left-radius: 7px;\n"
+            "    border-top-right-radius: 7px;\n"
+            "     subcontrol-position: top;\n"
+            "     subcontrol-origin: margin;\n"
+            " }\n"
+            " QScrollBar::up-arrow:vertical, QScrollBar::down-arrow:vertical {\n"
+            "     background: none;\n"
+            " }\n"
+            "\n"
+            " QScrollBar::add-page:vertical, QScrollBar::sub-page:vertical {\n"
+            "     background: none;\n"
+            " }\n")
+        self.ui_layers.setSelectionMode(QListWidget.NoSelection)
+        # Criando o layout
+        main_layout = QVBoxLayout(self)
+        main_layout.addWidget(self.ui_layers)
+
+    def add_new_layers(self):
+        dados = (['Arroz', 6, 25],
+                 ['Carne de pato', 2, 10], 
+                 ['Batatas', 5.8, 24], ['Pimenta', 2, 4],
+                 ['Alho', 1.2, 5], ['Cebola', 1, 5], )
+        for x in range(6):
+            widget = Block_low_stock(dados[x][0], dados[x][1], dados[x][2])
+            item = QListWidgetItem()
+            self.ui_layers.insertItem(self.ui_layers.count(), item)
+            self.ui_layers.setItemWidget(item, widget)
+            item.setSizeHint(widget.sizeHint())
+
 class Page_home(QWidget):
     def __init__(self):
         super(Page_home, self).__init__()
@@ -292,11 +429,12 @@ class Page_home(QWidget):
         self.estoque_em_baixa_title.setStyleSheet('color: #ffea65')
         self.estoque_em_baixa_title.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Fixed)
         self.estoque_em_baixa_title.setAlignment(Qt.AlignHCenter)
-        self.estoque_em_baixa_blocos = QFrame()
+        self.estoque_em_baixa_blocos = Block_stock_manager()
+        self.estoque_em_baixa_blocos.add_new_layers()
         # Criando um layout para estoque_em_baixa_frame
         self.estoque_em_baixa_layout = QVBoxLayout(self.estoque_em_baixa_frame)
         self.estoque_em_baixa_layout.setSpacing(0)
-        self.estoque_em_baixa_layout.setContentsMargins(0, 5, 0, 0)
+        self.estoque_em_baixa_layout.setContentsMargins(0, 5, 0, 5)
         # Adicionaodo elementos a pedidos_ativos_layout
         self.estoque_em_baixa_layout.addWidget(self.estoque_em_baixa_title)
         self.estoque_em_baixa_layout.addWidget(self.estoque_em_baixa_blocos)
