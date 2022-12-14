7b9c881a75d97f6e2138c71e16a4ce04ec7c82b7
Clemente Alberto Cazadi <clementecazadi@gmail.com>
2022-12-14 12:19:10 +0100

    trabalhando no header

--- Português/meus projectos/Cgest2/ui/windows/caixa.py
+++ Português/meus projectos/Cgest2/ui/windows/caixa.py
@@ -1,5 +1,6 @@
 from PySide6.QtWidgets import QWidget, QFrame, QVBoxLayout, QSizePolicy, QHBoxLayout, QLabel, \
-    QProgressBar, QListWidget, QListWidgetItem, QScrollBar, QStackedWidget, QTreeWidget
+    QProgressBar, QListWidget, QListWidgetItem, QScrollBar, QStackedWidget, QTreeWidget, QTreeWidgetItem,\
+    QHeaderView
 from PySide6.QtCore import Qt
 from ui.custome.button import Button_custome_2
 
@@ -9,11 +10,13 @@ class caixa_page(QWidget):
         # Criando o layout principal.
         self.main_layout = QHBoxLayout(self)
         self.main_layout.setContentsMargins(15, 15, 15, 15)
+        self.main_layout.setSpacing(15)
         # Criando o layout secundario.
         self.left_layout = QVBoxLayout()
         # Criando a parte direito do layout para adicionar produtos e quantias.
         self.right_widget = QStackedWidget()
         self.right_widget.setStyleSheet('background-color: #000000')
+        self.right_widget.setMaximumWidth(350)
     
         # Adicionando o left_layout e right ao menu principal.
         self.main_layout.addLayout(self.left_layout)
@@ -29,6 +32,50 @@ class caixa_page(QWidget):
         self.facture = Button_custome_2(icon_path='facture.svg')
         self.cancel_bill = Button_custome_2(icon_path='cancel.svg', icon_color='#ff8080')
         self.product_list = QTreeWidget()
+        self.header = QHeaderView(Qt.Horizontal)
+        self.header.setDefaultSectionSize(200)
+        self.header.sectionR
+        self.header.setSectionsMovable(False)
+        self.product_list.setHeader(self.header)
+        self.product_list.setHeaderLabels(['Codigo', 'Nome do Prod.', 'Quantia', 'Preço'])
+        self.product_list.setStyleSheet(""
+        "QTreeWidget {	\n"
+        "	background-color: #0c0c0c;\n"
+        "	border-radius: 5px;\n"
+        "}\n"
+        "QTableWidget::item{\n"
+        "	border-color: rgb(44, 49, 60);\n"
+        "	padding-left: 5px;\n"
+        "	padding-right: 5px;\n"
+        "	gridline-color: rgb(44, 49, 60);\n"
+        "}\n"
+        "QTableWidget::item:selected{\n"
+        "	background-color: rgb(85, 170, 255);}\n"
+        "QHeaderView::section{\n"
+        "	Background-color: #35c2a6;\n"
+        "	width: 50px;\n"
+        "	border: 1px solid rgb(44, 49, 60);\n"
+        "	border-style: none;\n"
+        "    border-bottom: 1px solid rgb(44, 49, 60);\n"
+        "    border-right: 1px solid rgb(44, 49, 60);\n"
+        "}\n"
+        ""
+        "QTableWidget::horizontalHeader {	\n"
+        "	background-color: rgb(81, 255, 0);\n"
+        "}\n"
+        "QHeaderView::section:horizontal\n"
+        "{\n"
+        "    border: 1px solid rgb(32, 34, 42);\n"
+        "	background-color: #35c2a6;\n"
+        "   width: 10px;\n"
+        "   height: 30px;"
+        "   color: #ffffff;"
+        "	padding: 10px;\n"
+        "}\n"
+        "QHeaderView::section:vertical\n"
+        "{\n"
+        "    border: 1px solid rgb(44, 49, 60);\n"
+        "}\n")
 
         # Criando um layout para botões.
         self.left_button_layout = QHBoxLayout()
