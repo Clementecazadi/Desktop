76472d073cd4d5ed8f54a44c5189530d678fc24f
Clemente Alberto Cazadi <clementecazadi@gmail.com>
2022-12-18 07:31:15 +0100

    Aba Caixa
    Trabalhei na aba caixa adicionando lable e outros elementos.

--- Português/meus projectos/Cgest2/ui/windows/caixa.py
+++ Português/meus projectos/Cgest2/ui/windows/caixa.py
@@ -3,6 +3,7 @@ from PySide6.QtWidgets import QWidget, QFrame, QVBoxLayout, QSizePolicy, QHBoxLa
     QHeaderView
 from PySide6.QtCore import Qt
 from ui.custome.button import Button_custome_2
+from ui.custome.Label import get_font, AdvencedLabel
 
 class caixa_page(QWidget):
     def __init__(self):
@@ -15,7 +16,7 @@ class caixa_page(QWidget):
         self.left_layout = QVBoxLayout()
         # Criando a parte direito do layout para adicionar produtos e quantias.
         self.right_widget = QStackedWidget()
-        self.right_widget.setStyleSheet('background-color: #000000')
+        self.right_widget.setStyleSheet('background-color: #0c0c0c')
         self.right_widget.setMaximumWidth(350)
     
         # Adicionando o left_layout e right ao menu principal.
@@ -31,57 +32,10 @@ class caixa_page(QWidget):
         self.bill_wait_list = Button_custome_2(icon_path='list_bill.svg')
         self.facture = Button_custome_2(icon_path='facture.svg')
         self.cancel_bill = Button_custome_2(icon_path='cancel.svg', icon_color='#ff8080')
-        self.product_list = QTreeWidget()
-        self.header = QHeaderView(Qt.Horizontal)
-        self.header.setDefaultSectionSize(200)
-        self.header.sectionR
-        self.header.setSectionsMovable(False)
-        self.product_list.setHeader(self.header)
-        self.product_list.setHeaderLabels(['Codigo', 'Nome do Prod.', 'Quantia', 'Preço'])
-        self.product_list.setStyleSheet(""
-        "QTreeWidget {	\n"
-        "	background-color: #0c0c0c;\n"
-        "	border-radius: 5px;\n"
-        "}\n"
-        "QTableWidget::item{\n"
-        "	border-color: rgb(44, 49, 60);\n"
-        "	padding-left: 5px;\n"
-        "	padding-right: 5px;\n"
-        "	gridline-color: rgb(44, 49, 60);\n"
-        "}\n"
-        "QTableWidget::item:selected{\n"
-        "	background-color: rgb(85, 170, 255);}\n"
-        "QHeaderView::section{\n"
-        "	Background-color: #35c2a6;\n"
-        "	width: 50px;\n"
-        "	border: 1px solid rgb(44, 49, 60);\n"
-        "	border-style: none;\n"
-        "    border-bottom: 1px solid rgb(44, 49, 60);\n"
-        "    border-right: 1px solid rgb(44, 49, 60);\n"
-        "}\n"
-        ""
-        "QTableWidget::horizontalHeader {	\n"
-        "	background-color: rgb(81, 255, 0);\n"
-        "}\n"
-        "QHeaderView::section:horizontal\n"
-        "{\n"
-        "    border: 1px solid rgb(32, 34, 42);\n"
-        "	background-color: #35c2a6;\n"
-        "   width: 10px;\n"
-        "   height: 30px;"
-        "   color: #ffffff;"
-        "	padding: 10px;\n"
-        "}\n"
-        "QHeaderView::section:vertical\n"
-        "{\n"
-        "    border: 1px solid rgb(44, 49, 60);\n"
-        "}\n")
-
         # Criando um layout para botões.
         self.left_button_layout = QHBoxLayout()
         self.left_button_layout.setContentsMargins(0, 0, 0, 0)
         self.left_button_layout.setSpacing(10)
-
         # Adicionando os botões para o layout
         self.left_button_layout.addWidget(self.dining_table)
         self.left_button_layout.addWidget(self.product)
@@ -91,19 +45,122 @@ class caixa_page(QWidget):
         self.left_button_layout.addWidget(self.bill_wait_list)
         self.left_button_layout.addWidget(self.facture)
         self.left_button_layout.addWidget(self.cancel_bill)
-        
+
+        self.product_list = QTreeWidget()
+        self.header = QHeaderView(Qt.Horizontal)
+        self.header.setFont(get_font('JosefinSans-SemiBold.ttf', 18))
+        #self.header.setSectionsMovable(False)
+        self.header.setSectionResizeMode(QHeaderView.Stretch)
+        self.product_list.setHeader(self.header)
+        self.product_list.setHeaderLabels(['Codigo', 'Nome do Prod.', 'Quantia', 'Preço'])
+        self.product_list.setStyleSheet(""
+            " QScrollBar:vertical {\n"
+            "	border: none;\n"
+            "    background: rgb(52, 59, 72);\n"
+            "    width: 14px;\n"
+            "    margin: 21px 0 21px 0;\n"
+            "	border-radius: 0px;\n"
+            " }\n"
+            " QScrollBar::handle:vertical {	\n"
+            "	background: #9955ff;\n"
+            "    min-height: 25px;\n"
+            "	border-radius: 7px\n"
+            " }\n"
+            " QScrollBar::add-line:vertical {\n"
+            "     border: none;\n"
+            "    background: rgb(55, 63, 77);\n"
+            "     height: 20px;\n"
+            "	border-bottom-left-radius: 7px;\n"
+            "    border-bottom-right-radius: 7px;\n"
+            "     subcontrol-position: bottom;\n"
+            "     subcontrol-origin: margin;\n"
+            " }\n"
+            " QScrollBar::sub-line:vertical {\n"
+            "	border: none;\n"
+            "    background: rgb(55, 63"
+                                    ", 77);\n"
+            "     height: 20px;\n"
+            "	border-top-left-radius: 7px;\n"
+            "    border-top-right-radius: 7px;\n"
+            "     subcontrol-position: top;\n"
+            "     subcontrol-origin: margin;\n"
+            " }\n"
+            " QScrollBar::up-arrow:vertical, QScrollBar::down-arrow:vertical {\n"
+            "     background: none;\n"
+            " }\n"
+            "\n"
+            " QScrollBar::add-page:vertical, QScrollBar::sub-page:vertical {\n"
+            "     background: none;\n"
+            " }\n"
+        "QTreeWidget {	\n"
+        "   color: #ffffff;"
+        "   text-align: center;"
+        "	background-color: #0c0c0c;\n"
+        "	border-radius: 5px;\n"
+        "}\n"
+        "QTreeWidget::item{\n"
+        "   height:35px;"
+        "	padding-left: 5px;\n"
+        #"	padding-right: 5px;\n"
+        "}\n"
+        "QTreeWidget::item:selected{\n"
+        "	background-color: #9955ff;}\n"
+        "QHeaderView::section{\n"
+        "	Background-color: #35c2a6;\n"
+        #"	width: 50px;\n"
+        #"	border: 7px solid rgb(44, 49, 60);\n"
+        "	border-style: none;\n"
+        "}\n"
+        ""
+        "QTableWidget::horizontalHeader {	\n"
+        "	background-color: rgb(81, 255, 0);\n"
+        "}\n"
+        "QHeaderView::section:horizontal\n"
+        "{\n"
+        "   border: 1px solid rgb(32, 34, 42);\n"
+        "	background-color: #35c2a6;\n"
+        "   width: 10px;\n"
+        "   height: 30px;"
+        "   color: #ffffff;"
+        "	padding: 10px;\n"
+        "}\n")
+        self.product_list.setFont(get_font('JosefinSans-SemiBold.ttf', 14))
+        listas = [['01911', 'Carne de porco', '2', '3600 Kzs'], 
+        ['45432', 'Sandwiche de frango', '1', '2200 Kzs'], ['34213', 'Feigoado de borco', '2', '3450 Kzs']]
+        for x in range(3):
+            item = QTreeWidgetItem(self.product_list, listas[x])
+            for x in range(4):
+                item.setTextAlignment(x, Qt.AlignCenter)
+                item.setToolTip(1, item.text(1))
+
+        self.purchase_details_main_layout = QHBoxLayout()
+        self.purchase_details_main_layout.setContentsMargins(0, 0, 0, 0)
+        self.purchase_details_main_layout.setSpacing(0)
+
+        self.purchase_details_layout_1 = QVBoxLayout()
+        self.purchase_details_layout_1.setContentsMargins(0, 0, 0, 0)
+        self.purchase_details_layout_1.setSpacing(10)
+        self.dining_table_lable = AdvencedLabel('Mesa: 6', font_size=16)
+        self.total_price_lable = AdvencedLabel('Preço total: 9250 Kzs', font_size=16)
+        self.tax_lable = AdvencedLabel('Iva: 14%', font_size=16)
+        self.purchase_details_layout_1.addWidget(self.dining_table_lable)
+        self.purchase_details_layout_1.addWidget(self.total_price_lable)
+        self.purchase_details_layout_1.addWidget(self.tax_lable)
+
+        self.purchase_details_layout_2 = QVBoxLayout()
+        self.purchase_details_layout_2.setContentsMargins(0, 0, 0, 0)
+        self.purchase_details_layout_2.setSpacing(10)
+        self.final_price = AdvencedLabel('Preço Final: 10345 Kzs', font_size=16)
+        self.cliente_money_lable = AdvencedLabel('Dinheiro: 11000 Kzs', font_size=16)
+        self.customer_change_lable = AdvencedLabel('Troco do cliente: 655 Kzs', font_size=16)
+        self.purchase_details_layout_2.addWidget(self.final_price)
+        self.purchase_details_layout_2.addWidget(self.cliente_money_lable)
+        self.purchase_details_layout_2.addWidget(self.customer_change_lable)
 
 
-        
-
-
-        
+        self.purchase_details_main_layout.addLayout(self.purchase_details_layout_1)
+        self.purchase_details_main_layout.addLayout(self.purchase_details_layout_2)
+        # Adicionado Elmenetos ao layout left_layout 
         self.left_layout.addLayout(self.left_button_layout)
         self.left_layout.addWidget(self.product_list)
-        
-
-
-
-        
-        
-        
+        self.left_layout.addLayout(self.purchase_details_main_layout)
