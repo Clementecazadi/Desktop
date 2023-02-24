29d83fd5b5943ec3df89434f3299927051500923
Clemente Alberto Cazadi <clementecazadi@gmail.com>
2023-02-24 12:19:11 +0100

    Create ex1.py
    primeira aula

---
+++ Português/meu aprendisado/Bot_telegram/aula2/ex1.py
@@ -0,0 +1,13 @@
+familia = ['clemente', 'Joana', 'andre']
+familia.pop(0)
+familia.append(9)
+familia2 = list(('clemente', 'beto'))
+print(familia, familia2)
+trabalho = {'chef': 'Andre', 'secr': 'tabita'}
+trab = dict(chef = 'Andre', secr = 'tabita')
+casa = list((('Tv', True), ('Piscina', True), ('Chalé', False)))
+casa1 = dict(casa)
+print(trabalho, trab, casa1)
+comida = dict()
+comida.update(Prato = 'Aroz de legumes', Sobremesa = 'Gelado')
+print(comida)
