fcda1628073bf4046fc7f3c7d9dee8157d6b1ca5
Clemente Alberto Cazadi <clementecazadi@gmail.com>
2023-07-11 00:43:13 +0100

    minha web
    froshims

--- Português/meu aprendisado/cs50/modulo9/froshims1/application.py
+++ Português/meu aprendisado/cs50/modulo9/froshims1/application.py
@@ -1,8 +1,16 @@
 import os
 from cs50 import SQL
 from flask import Flask, render_template, request, redirect
+from flask_mail import Mail, Message
 
 app = Flask(__name__)
+app.config["MAIL_DEFAULT_SENDER"] = os.getenv("MAIL_DEFAULT_SENDER")
+app.config["MAIL_PASSWORD"] = os.getenv("MAIL_PASSWORD")
+app.config["MAIL_PORT"] = 587
+app.config["MAIL_SERVER"] = "smtp.gmail.com"
+app.config["MAIL_USE_TLS"] = True
+app.config["MAIL_USERNAME"] = os.getenv("MAIL_USERNAME")
+mail = Mail(app)
 
 db = SQL("sqlite:///froshims.db")
 
@@ -23,17 +31,22 @@ def index():
 def register():
     name = request.form.get("name")
     sport = request.form.get("sport")
+    email = request.form.get("email")
     if not name:
         return render_template("error.html", message="Missing name.")
     elif not sport:
         return render_template('error.html', message="Missing sport.")
     elif sport not in SPORTS:
         return render_template('error.html', message="Choise only the sports avalable.")
+    elif not email:
+        return render_template('error.html', message="Missing e-mail.")
     # print(request.form.getlist("sport"))
 
     #REGISTERS[name] = sport
     #return render_template("registrants.html", registrants=REGISTERS)
-    db.execute("INSERT INTO registrants(name, sport) VALUES (?, ?)", name, sport)
+    db.execute("INSERT INTO registrants(name, email, sport) VALUES (?, ?, ?)", name, email, sport)
+    message = Message(f"{name}, you are registered!", sender=os.getenv("MAIL_USERNAME") ,recipients=[email])
+    mail.send(message)
     return redirect("/registrants")
 
 @app.route("/registrants")
