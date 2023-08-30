d65b4899368ea87296187a72f7c7ddf361a8d142
Clemente Alberto Cazadi <112099197+Clementecazadi@users.noreply.github.com>
2023-08-30 18:40:57 +0100

    Update aula9.0.py

--- Português/meu aprendisado/telegram_bot/aula9/aula9.0.py
+++ Português/meu aprendisado/telegram_bot/aula9/aula9.0.py
@@ -3,7 +3,7 @@ import telegram
 
 
 async def main():
-    TOKEN = "6157993125:AAHX38Zo9Cglqh3y7_-5DqysX0rrsN6tq28"
+    TOKEN =
     bot = telegram.Bot(TOKEN)
     async with bot:
         print(await bot.get_me())
